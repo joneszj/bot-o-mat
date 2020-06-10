b902f446bfce9e2e9ef2d907fa000497144c01ee
joneszj <joneszj@gmail.com>
2020-06-10 02:41:26 -0400

    create bot cta reactive form validation
    clean: replace ngModel actorTypeSelection with createBotFormGroup.get('botNameInput').value
    ngModel & reactive form controls will be deprecated in Angular 7
    
    learned: reactive forms, formGroup, formControl, AbstractControl methods
    
    todo: finish remaining form controls and submit form

--- Bot-O-Mat.Web.Angular/ClientApp/src/app/components/modals/createBot/createBot.component.ts
+++ Bot-O-Mat.Web.Angular/ClientApp/src/app/components/modals/createBot/createBot.component.ts
@@ -3,6 +3,7 @@ import { ActorService } from '../../../services/actorService.service';
 import { ActorType } from '../../../models/actorType';
 import { Gender } from '../../../models/Gender';
 import { RobotType } from '../../../models/robotType';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-bot',
@@ -15,6 +16,7 @@ export class CreateBotComponent {
   cyborGenders: Gender[];
   robotTypes: RobotType[];
   botTypeSelected: false;
+  createBotFormGroup: FormGroup;
 
   constructor(private actorService: ActorService) { }
 
@@ -22,5 +24,19 @@ export class CreateBotComponent {
     this.actorService.GetActorTypes().subscribe(actorTypes => this.actorTypes = actorTypes);
     this.actorService.GetRobotTypes().subscribe(robotTypes => this.robotTypes = robotTypes);
     this.actorService.GetCyborgGenders().subscribe(genders => this.cyborGenders = genders);
+
+    // note: https://stackoverflow.com/a/51343849
+    // formGroups are not designed to be bound to component models
+    // use the AbstractControl methods to retrieve values for comsumption, though default values can be set here
+    this.createBotFormGroup = new FormGroup({
+      // TODO: optional property ?. breaks compilation
+      botTypeSelect: new FormControl(null, Validators.required),
+      botNameInput: new FormControl(null, [Validators.required, Validators.maxLength(20)])
+    });
+    this.createBotFormGroup.valueChanges.subscribe(v => console.log(v))
+  }
+
+  test = () => {
+    console.log(this.createBotFormGroup.getRawValue());
   }
 }
